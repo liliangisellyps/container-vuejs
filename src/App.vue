0e998898a5c7bf91f0ccaf178c20b7e7aa37d423
Lilian Giselly <lgps@ic.ufal.br>
2020-09-22 01:56:39 -0300

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,89 @@
 <template>
   <div id="app">
-    <div class="container">
-      <h1>TODO VUE!</h1>
-      <input
-        class="input"
-        v-model="newToDo"
-      />
-      <button class="add-button">Add</button>
-      <ul style="list-style-type: none;">
-        <li v-for="(todo, i) in existingToDo" v-bind:key="todo.id">
-          <span>
-            {{i+1}}: {{todo.text}}
-          </span>
-        </li>
-      </ul>
-    </div>
+    <Container>
+      <h2 slot="title">Usuários</h2>
+      <button slot="action" class="button">Adicionar</button>
+      <table slot="body" style="width:100%">
+      <tr>
+        <th>Nome</th>
+        <th>Campo 1</th>
+        <th>Campo 2</th>
+        <th>Campo 3</th>
+        <th>Campo 4</th>
+      </tr>
+      <tr>
+        <td>Fulano</td>
+        <td>Campo 1</td>
+        <td>Campo 2</td>
+        <td>Campo 3</td>
+        <td>Campo 4</td>
+      </tr>
+      <tr>
+        <td>Fulano</td>
+        <td>Campo 1</td>
+        <td>Campo 2</td>
+        <td>Campo 3</td>
+        <td>Campo 4</td>
+      </tr>
+      <tr>
+        <td>Fulano</td>
+        <td>Campo 1</td>
+        <td>Campo 2</td>
+        <td>Campo 3</td>
+        <td>Campo 4</td>
+      </tr>
+      <tr>
+        <td>Fulano</td>
+        <td>Campo 1</td>
+        <td>Campo 2</td>
+        <td>Campo 3</td>
+        <td>Campo 4</td>
+      </tr>
+    </table>
+    </Container>
   </div>
 </template>
 
 <script>
+import Container from '@/components/Container';
 
 export default {
   name: 'App',
-  data(){
-    return {
-      newToDo: '',
-      existingToDo: [
-        {text: 'cry', id:0},
-        {text: 'sleep', id: 1},
-        {text: 'eat', id: 2}
-      ]
-    }
-  },
-  method: {
-    add(){
-      this.existingToDo.push({
-        text: this.newToDo,
-        id: new Date().valueOf()
-      }),
-      THIS.
-    }
-  }
+  components: {Container}
 }
 </script>
 
 <style>
-
+  .button {
+    background-color: #fab400; /* Green */
+    border: none;
+    border-radius: 12px;
+    padding: 10px 35px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 20px;
+    font-family: 'Open Sans', sans-serif;
+  }
+  table {
+    border-spacing: 0;
+    border-collapse: collapse;
+    border: 1px solid #b4b4b4;
+    margin-top: 15px;
+  }
+  th {
+    text-align: left;
+    font-weight: 100;
+    background-color: #fff;
+    border-bottom: 1px solid #eee;
+  }
+  th, td {
+    padding: 15px;
+  }
+  tr:nth-child(even) {
+    background-color: #fff;
+  }
+  tr:nth-child(odd) {
+    background-color: #eee;
+  }
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+      <div class="header">
+          <slot name="title"></slot>
+          <slot name="action"></slot>
+      </div>
+      <div class="body">
+          <slot name="body"></slot>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Container'
+}
+</script>
+
+<style>
+  .container {
+    font-family: 'Open Sans', sans-serif;
+    padding: 30px;
+    border-radius: 12px;
+  }
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #a6a6a6;
+  }
+  
+</style>
